8f62ecfd334177da3f1667139068b41e358affaf
Kegan Smith <kegansmith415@gmail.com>
2022-03-11 14:33:39 -0800

    Something went very wrong when trying to merge the rps-ui branch, so I just copied over the .js and .html files from the branch into the mangled mess that was the main. Lets hope this works

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection = '';
@@ -42,8 +41,6 @@ computerResults.classList.add('resultDisplay');
 computerResults.textContent = `Computer\'s score: ${computerScore}`;
 container.appendChild(computerResults);
 
-=======
->>>>>>> 8acc144b2e5b1eea1b334d33525aa2bf309b9817
 function computerPlay () { //Takes the random number between 1 and 3 and returns rock/paper/scissors
     let randomNumber = Math.floor(Math.random()*3)+1; //Selects a random number between 1 and 3
     if (randomNumber === 1) {
@@ -55,11 +52,7 @@ function computerPlay () { //Takes the random number between 1 and 3 and returns
     }
 }
 
-<<<<<<< HEAD
 function playRound (playerSelection, computerSelection) { //Determines the winner of a single round
-=======
-function playRound (playerSelection, computerSelection) {
->>>>>>> 8acc144b2e5b1eea1b334d33525aa2bf309b9817
     if (computerSelection === 'ROCK' && playerSelection === 'ROCK') {
         return 'Rock vs. rock, it\'s a tie!';
     } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER') {
@@ -77,16 +70,11 @@ function playRound (playerSelection, computerSelection) {
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         return 'Scissors beats paper, you lose!';
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'SCISSORS') {
-<<<<<<< HEAD
         return 'Scissors vs. scissors, it\'s a tie!';
-=======
-        return 'Scissosrs vs. scissors, it\'s a tie!';
->>>>>>> 8acc144b2e5b1eea1b334d33525aa2bf309b9817
     }
 }
 
 function game() {
-<<<<<<< HEAD
     if (playerSelection === computerSelection) { //If the result is a tie and the round repeats
         console.log("This round was a tie! Player's score: " + playerScore + ", Computer's score: " + computerScore);
     } else if ((computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
@@ -118,47 +106,3 @@ function checkWinner () { //Alerts the winner and disables buttons
         scissorsBtn.disabled = true;
     }
 }
-=======
-    let playerScore = 0;
-    let computerScore = 0
-    let repeated = false;
-    alert("Ready for a game of Rock Paper Scissors? Let's play best 3 out of 5!");
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, paper, or scissors?").toUpperCase();
-        const computerSelection = computerPlay();
-        console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
-        if (playerSelection === computerSelection) {
-            //If the result is a tie and the round repeats
-            repeated = true;
-            i--;
-            console.log("This round was a tie! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-        } else if ((computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
-                    (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') || 
-                    (computerSelection === 'SCISSORS' && playerSelection === 'ROCK')) {
-            //If player wins a round
-            playerScore ++;
-            console.log("You won this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-        } else if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
-                    (computerSelection === 'PAPER' && playerSelection === 'ROCK') || 
-                    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
-            //If player loses a round
-            computerScore ++;
-            console.log("You lost this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-        }
-        if (parseInt(playerScore) === 3) {
-            console.log("Congratulations! You have won!");
-            break;
-        } else if (parseInt(computerScore) === 3) {
-            console.log("You have lost the game!");
-            break;
-        }
-    }
-}
-
-game();
-
-//changes I have made to solve problem of not having specified rounds:
-    //moved the randomNumber variable inside computerPlay
-    //moved the playerSelection and computerSelection inside the for loop (which really makes sense to do)
->>>>>>> 8acc144b2e5b1eea1b334d33525aa2bf309b9817
