42d3b1686ed15945f8241edf6e4ac827012af859
Kegan Smith <kegansmith415@gmail.com>
2022-03-11 13:59:01 -0800

    Buttons functional, modified game fxn, created checkWinner() to disable buttons when game ends

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = '';
+let computerSelection;
+
+checkWinner(); 
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+rockBtn.addEventListener('click', ()=>{ //to call fxn w/ parameter in a event listener, use empty fxn to call fxn
+    computerSelection = computerPlay();
+    playerSelection = 'ROCK';
+    playRound(playerSelection, computerSelection);
+    game();
+}); 
+paperBtn.addEventListener('click', ()=>{ 
+    computerSelection = computerPlay();
+    playerSelection = 'PAPER';
+    playRound(playerSelection, computerSelection);
+    game();
+});
+scissorsBtn.addEventListener('click', ()=>{ 
+    computerSelection = computerPlay();
+    playerSelection = 'SCISSORS';
+    playRound(playerSelection, computerSelection);
+    game();
+});
+
+
+
+//Scoreboard
+const container = document.querySelector("#container");
+const playerResults = document.createElement('div');
+playerResults.classList.add('resultDisplay');
+playerResults.textContent = `Player\'s score: ${playerScore}`;
+container.appendChild(playerResults);
+const computerResults = document.createElement('div');
+computerResults.classList.add('resultDisplay');
+computerResults.textContent = `Computer\'s score: ${computerScore}`;
+container.appendChild(computerResults);
 
 function computerPlay () { //Takes the random number between 1 and 3 and returns rock/paper/scissors
     let randomNumber = Math.floor(Math.random()*3)+1; //Selects a random number between 1 and 3
@@ -33,68 +73,34 @@ function playRound (playerSelection, computerSelection) {
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0
-    let winnersScore = 0;
-//    for (let i = 0; i < 5; i++) {
-        const computerSelection = computerPlay();
-        let playerSelection = '';
-        const rockBtn = document.querySelector('#rock');
-        const paperBtn = document.querySelector('#paper');
-        const scissorsBtn = document.querySelector('#scissors');
-        rockBtn.addEventListener('click', ()=>{ 
-            playerSelection = 'ROCK';
-            playRound(playerSelection, computerSelection);
-            console.log('Button click: ' + playerSelection);
-        }); //to call fxn w/ parameter in a event listener, use empty fxn to call fxn
-        paperBtn.addEventListener('click', ()=>{ 
-            playerSelection = 'PAPER';
-            playRound(playerSelection, computerSelection);
-            console.log('Button click: ' + playerSelection);
-        });
-        scissorsBtn.addEventListener('click', ()=>{ 
-            playerSelection = 'SCISSORS';
-            playRound(playerSelection, computerSelection);
-            console.log('Button click: ' + playerSelection);
-        });
-        if (playerSelection === computerSelection) {
-            //If the result is a tie and the round repeats
-//            i--;
-            console.log("This round was a tie! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-        } else if ((computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
-                    (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') || 
-                    (computerSelection === 'SCISSORS' && playerSelection === 'ROCK')) {
-            //If player wins a round
-            playerScore ++;
-            console.log("You won this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-            appendResults();
-        } else if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
-                    (computerSelection === 'PAPER' && playerSelection === 'ROCK') || 
-                    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
-            //If player loses a round
-            computerScore ++;
-            console.log("You lost this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
-            appendResults();
-        }
+    if (playerSelection === computerSelection) { //If the result is a tie and the round repeats
+        console.log("This round was a tie! Player's score: " + playerScore + ", Computer's score: " + computerScore);
+    } else if ((computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
+                (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') || 
+                (computerSelection === 'SCISSORS' && playerSelection === 'ROCK')) { //If player wins a round
+        playerScore ++;
+        playerResults.textContent = `Player\'s score: ${playerScore}`;
+        checkWinner();
+        console.log("You won this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
+    } else if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
+                (computerSelection === 'PAPER' && playerSelection === 'ROCK') || 
+                (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) { //If player loses a round
+        computerScore ++;
+        computerResults.textContent = `Computer\'s score: ${computerScore}`;
+        checkWinner();
+        console.log("You lost this round! Player's score: " + playerScore + ", Computer's score: " + computerScore);
     }
-//}
-
-function appendResults () {
-    const container = document.querySelector("#container");
-    const playerResults = document.createElement('div');
-    playerResults.classList.add('resultDisplay');
-    playerResults.textContent = `Player\'s score: ${playerScore}`;
-    container.appendChild(playerResults);
-    const computerResults = document.createElement('div');
-    computerResults.classList.add('resultDisplay');
-    computerResults.textContent = `Computer\'s score: ${computerScore}`;
-    container.appendChild(computerResults);
 }
 
-game();
-
-//changes I have made to solve problem of not having specified rounds:
-    //moved the randomNumber variable inside computerPlay
-    //moved the playerSelection and computerSelection inside the for loop (which really makes sense to do)
-
-
+function checkWinner () { //Alerts the winner and disables buttons
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) {
+            alert(`Congratulations! You have won the game! Final score: Player: ${playerScore}, Computer: ${computerScore}`);
+        } else {
+            alert(`You have lost! Final score: Player: ${playerScore}, Computer: ${computerScore}`);
+        }
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+    }
+}
